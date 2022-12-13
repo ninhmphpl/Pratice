4610c14aa40e2947882de32e5b161936473d661c
NinhPc <ninhmphpl@gmail.com>
2022-12-13 15:00:39 +0700

    update time

--- demo10_view_clock/src/main/java/com/example/demo10/controller/TimeController.java
+++ demo10_view_clock/src/main/java/com/example/demo10/controller/TimeController.java
@@ -14,19 +14,11 @@ public class TimeController {
 
     @GetMapping("/world-clock")
     public String getTimeByTimezone(ModelMap model, @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
-        // Lấy ra thời gian hiện tại
-        Date date = new Date();
-        // Lấy ra time zone hiện tại
-        TimeZone local = TimeZone.getDefault();
-        // Lấy ra time zone của 1 thành phố cụ thể
-        TimeZone locale = TimeZone.getTimeZone(city);
-
-        // Tính thời gian hiện tại của một thành phố cụ thể
-        long locale_time = date.getTime() +
-                (locale.getRawOffset() - local.getRawOffset());
-        // Cài đặt lại thời gian cho biến date thành thời gian hiện tại của 1 thành phố cụ thể
-        date.setTime(locale_time);
-
+        Date date = new Date();// Lấy ra thời gian hiện tại
+        TimeZone local = TimeZone.getDefault();// Lấy ra time zone hiện tại
+        TimeZone locale = TimeZone.getTimeZone(city);// Lấy ra time zone của 1 thành phố cụ thể
+        long locale_time = date.getTime() + (locale.getRawOffset() - local.getRawOffset());// Tính thời gian hiện tại của một thành phố cụ thể
+        date.setTime(locale_time);// Cài đặt lại thời gian cho biến date thành thời gian hiện tại của 1 thành phố cụ thể
         model.addAttribute("city", city);
         model.addAttribute("date", date);
         return "index";
